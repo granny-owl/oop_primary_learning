4a35303c696ddcd31bbdda481dae1f50fa9bb2db
granny-owl <70903146+granny-owl@users.noreply.github.com>
2020-10-13 14:00:05 +0300

    Add files via upload

---
+++ oop_cat_dog.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+struct animal {
+    virtual void sound() {
+
+    }
+};
+
+struct dog : public animal {
+    void sound() override {
+        std::cout << "bark\n";
+    }
+};
+
+struct cat : public animal {
+    void sound() override {
+        std::cout << "meow\n";
+    }
+};
+
+int main()
+{
+    dog doggo;
+    cat catto;
+    catto.sound();
+    doggo.sound();
+    return 0;
+}
+
