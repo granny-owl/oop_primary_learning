260fbab6eced138da362b9fcbacd81a8c4508f1a
granny-owl <70903146+granny-owl@users.noreply.github.com>
2020-10-15 13:39:29 +0300

    Add files via upload
    here we create a protected member function 'print()' in the base class 'animal' and then call it in a public member function 'say' which we first declare in 'animal' and then override in the derived classes 'cat' and 'dog'. the 'print()' function gets a string and couts it

---
+++ oop_animal_protected.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+class animal {
+public:
+    virtual void say() {}
+protected:
+    virtual void print(std::string str) {
+        std::cout << str;
+    }
+};
+
+class dog : public animal {
+public:
+    void say() override {
+        print("bark\n");
+    }
+};
+
+class cat : public animal {
+public:
+    void say() override {
+        print("meow\n");
+    }
+};
+
+int main()
+{
+    cat catto;
+    dog doggo;
+    catto.say();
+    doggo.say();
+    return 0;
+}
+
